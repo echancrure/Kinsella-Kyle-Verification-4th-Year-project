d4ff3112acf18db8df0871d7ec02e4db1fbc89dd
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-10-21 10:25:30 +0100

    I have added more tests

--- Task1/MainTest.java
+++ Task1/MainTest.java
@@ -1,4 +1,8 @@
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -37,5 +41,50 @@ class MainTest {
         Period p1 = new Period(start, end);
 
         assertEquals(p1.duration(), 2);
+    }
+
+
+    @Test
+    void checkSameTime() {
+        int start = 4;
+        int end = 4;
+
+        Period p1 = new Period(start, end);
+
+        assertEquals(p1.duration(), 0);
+    }
+
+    @Test
+    void invalidPeriod() {
+        Period p1 = new Period(-1, 0);
+        assertEquals(p1.duration(), -1);
+    }
+
+    @Test
+    void invalidDuration() {
+        int start = 5;
+        int end = 12;
+
+        Period period = new Period(start, end);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            period.duration();
+        });
+
+        assertEquals(period.duration(), -7, e.getMessage());
+    }
+
+    @Test
+    void startAndEndHourNotEqualDuration() {
+        int start = 2;
+        int end = 2;
+
+        Period period = new Period(start, end);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            period.duration();
+        });
+
+        assertEquals(period.duration(), 6, e.getMessage());
     }
 }
